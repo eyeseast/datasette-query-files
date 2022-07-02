eadb0d094b248780acd6b70ec5eb81acec4d7519
Chris Amico <eyeseast@gmail.com>
2022-07-02 16:15:03 -0400

    Writable queries. Closes #2.

---
+++ tests/queries/writable/write_message.sql
@@ -0,0 +1,4 @@
+INSERT INTO
+    messages (user_id, message, datetime)
+VALUES
+    (:_actor_id, :message, :_now_datetime_utc)
--- tests/test_query_files.py
+++ tests/test_query_files.py
@@ -1,10 +1,13 @@
 import pathlib
 import pytest
+import sqlite3
+import httpx
 from datasette.app import Datasette
+from datasette.app import DatasetteClient
 
 PLUGIN_NAME = "datasette-query-files"
 TESTS = pathlib.Path(__file__).parent
-DATABASE = TESTS / "legislators.db"
+LEGISLATORS = TESTS / "legislators.db"
 QUERIES = TESTS / "queries"
 
 SQL_FILES = {
@@ -17,12 +20,24 @@ METADATA = {
     "women_senators": {"title": "Women in the Senate"},
 }
 
+CREATE_WRITE_TABLE = """
+create table messages (user_id text, message text, datetime text);
+"""
+
 
 @pytest.fixture
-def ds():
-    return Datasette(
-        [DATABASE], metadata={"plugins": {PLUGIN_NAME: {"query_directory": QUERIES}}}
+def ds(tmp_path):
+    WRITABLE = tmp_path / "writable.db"
+
+    writable = sqlite3.connect(WRITABLE)
+    writable.executescript(CREATE_WRITE_TABLE)
+
+    yield Datasette(
+        [LEGISLATORS, WRITABLE],
+        metadata={"plugins": {PLUGIN_NAME: {"query_directory": QUERIES}}},
     )
+
+    WRITABLE.unlink()
 
 
 @pytest.mark.asyncio
@@ -74,3 +89,49 @@ async def test_query_metadata(ds):
         query = queries[name]
 
         assert metadata["title"] == query["title"]
+
+
+@pytest.mark.asyncio
+async def test_write_message(ds):
+    assert ds._root_token is not None
+    token = ds._root_token
+    async with httpx.AsyncClient(
+        app=ds.app(),
+        base_url="http://localhost",
+        cookies={"ds_actor": ds.sign({"a": {"id": "root"}}, "actor")},
+    ) as client:
+
+        url = ds.urls.query("writable", "write_message")
+        r1 = await client.get(url)
+        assert 200 == r1.status_code
+
+        csrftoken = r1.cookies["ds_csrftoken"]
+        r2 = await client.post(
+            url,
+            data={
+                "message": "Hello, world!",
+                "csrftoken": csrftoken,
+            },
+            headers={"Accept": "application/json"},
+        )
+
+        assert 200 == r2.status_code
+
+        data = r2.json()
+
+        assert data["ok"]
+
+
+@pytest.mark.asyncio
+async def _test_write_message(ds):
+    cookie = ds.sign({"a": {"id": "test"}}, "actor")
+    url = ds.urls.query("writable", "write_message")
+
+    resp = await ds.client.post(
+        url,
+        data={"message": "Hello, world!"},
+        headers={"Accept": "application/json"},
+        cookies={"ds_actor": cookie},
+    )
+
+    assert 200 == resp.status_code
